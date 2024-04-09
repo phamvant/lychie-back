3c344f38af5a7baf9be0f31554dac1eb0f477072
phamvant <pham.t.286.01@gmail.com>
2024-04-10 01:09:47 +0900

    modify discount schema

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,14 +1,23 @@
-import { IsArray, IsObject, IsString } from "class-validator";
+import { IsArray, IsNumber, IsObject, IsString } from "class-validator";
 
 export class CreateProductDto {
   @IsString()
   productName: string;
   @IsString()
   productDescription: string;
+  //-------PRICE--------//
+  @IsNumber()
+  productCostPrice: number;
+  @IsNumber()
+  productPrice: number;
+  @IsNumber()
+  productFinalPrice: number;
+  //-------DISCOUNT-----//
   @IsString()
-  productCostPrice: string;
-  @IsString()
-  productPrice: string;
+  productDiscountType: string;
+  @IsNumber()
+  productDiscountAmount?: number;
+  //-------VARIANT------//
   @IsString()
   productCategory: string;
   @IsString()
